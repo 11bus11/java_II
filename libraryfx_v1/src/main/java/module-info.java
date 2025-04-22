9f629edf51b3b681701eae6fb094a3d28df86452
Nicole <nicagu-3@student.ltu.se>
2025-04-22 13:15:50 -0300

    rfgdgd

--- libraryfx_v1/src/main/java/module-info.java
+++ libraryfx_v1/src/main/java/module-info.java
@@ -1,6 +1,8 @@
 module library {
-    requires javafx.controls;
+    requires javafx.controls; //drar in graphics för modulen
     requires javafx.fxml;
+
+    requires transitive javafx.graphics; //exporterar modul vidare
 
     opens library to javafx.fxml;
     exports library;
