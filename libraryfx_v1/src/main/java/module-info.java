6db1a63d946cb7e8749dce8d7815b5beea26ba6c
11bus11 <02linfor01@gmail.com>
2025-04-28 22:40:24 +0200

    figured out how jfx works

--- libraryfx_v1/src/main/java/module-info.java
+++ libraryfx_v1/src/main/java/module-info.java
@@ -1,10 +1,11 @@
 module library {
     requires javafx.controls;
     requires javafx.fxml;
+    requires transitive javafx.graphics;
     requires java.sql;
     requires com.zaxxer.hikari;
 
-    opens library to javafx.fxml, java.sql, com.zaxxer.hikari;
+    opens library to javafx.fxml, java.sql, com.zaxxer.hikari, javafx.graphics;
    
     exports library;
 }
