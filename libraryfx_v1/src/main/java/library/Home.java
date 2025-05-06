59137e11a4b6a9ce5210e654a16229dabcdbaa4b
Nicole <nicagu-3@student.ltu.se>
2025-05-06 20:51:59 -0300

    Home

---
+++ libraryfx_v1/src/main/java/library/Home.java
@@ -0,0 +1,33 @@
+package library;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+
+public class Home {
+
+    @FXML
+    private Button btnHome;
+
+    @FXML
+    private Button btnMyLibraryAccount;
+
+    @FXML
+    private Button btnSearch;
+
+    @FXML
+    void goToHome(MouseEvent event) {
+
+    }
+
+    @FXML
+    void goToMyLibraryAccount(MouseEvent event) {
+
+    }
+
+    @FXML
+    void goToSearch(MouseEvent event) {
+
+    }
+
+}
