207163efc52b15fb8769299a4bc866592ea04b80
Nicole <nicagu-3@student.ltu.se>
2025-05-06 21:14:14 -0300

    Search

---
+++ libraryfx_v1/src/main/java/library/SearchController.java
@@ -0,0 +1,44 @@
+
+package library;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+
+public class SearchController {
+
+    @FXML
+    private Button btnHome;
+
+    @FXML
+    private TableColumn<?, ?> colBarcode;
+
+    @FXML
+    private TableColumn<?, ?> colDescription;
+
+    @FXML
+    private TableColumn<?, ?> colISBN;
+
+    @FXML
+    private TableColumn<?, ?> colStatus;
+
+    @FXML
+    private TableColumn<?, ?> colTitle;
+
+    @FXML
+    private TableColumn<?, ?> colType;
+
+    @FXML
+    private TableColumn<?, ?> colYear;
+
+    @FXML
+    private TextField tfSearch;
+
+    @FXML
+    void goToHome(MouseEvent event) {
+
+    }
+
+}
