7f532cd9e93285f0a1ba86634d78b5ad92914d1a
Nicole <nicagu-3@student.ltu.se>
2025-05-06 21:21:28 -0300

    Account

---
+++ libraryfx_v1/src/main/java/library/MyLibraryAccount.java
@@ -0,0 +1,41 @@
+package library;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+
+public class MyLibraryAccount {
+
+    @FXML
+    private Button btnHome;
+
+    @FXML
+    private Button btnLoan;
+
+    @FXML
+    private Button btnMyLoans;
+
+    @FXML
+    private Button btnReturnLoan;
+
+    @FXML
+    void goToHome(MouseEvent event) {
+
+    }
+
+    @FXML
+    void goToLoan(MouseEvent event) {
+
+    }
+
+    @FXML
+    void goToMyLoans(MouseEvent event) {
+
+    }
+
+    @FXML
+    void goToReturnLoan(MouseEvent event) {
+
+    }
+
+}
