3f89ff548faefdb2992da4de8359f8ffa6f015d6
Nicole <nicagu-3@student.ltu.se>
2025-05-06 21:10:59 -0300

    DelayedList

---
+++ libraryfx_v1/src/main/java/library/DelayedListController.java
@@ -0,0 +1,45 @@
+
+package library;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+
+public class DelayedListController {
+
+    @FXML
+    private Button btnReturn;
+
+    @FXML
+    private TableColumn<?, ?> colBarcode;
+
+    @FXML
+    private TableColumn<?, ?> colDaysDelayed;
+
+    @FXML
+    private TableColumn<?, ?> colEmail;
+
+    @FXML
+    private TableColumn<?, ?> colTitle;
+
+    @FXML
+    private TableColumn<?, ?> colType;
+
+    @FXML
+    private TableColumn<?, ?> colYear;
+
+    @FXML
+    private TextField tfSearch;
+
+    @FXML
+    private TableView<?> tvDelayedList;
+
+    @FXML
+    void goToStaff(MouseEvent event) {
+
+    }
+
+}
