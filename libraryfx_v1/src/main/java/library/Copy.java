635379112c8fe4f7530d46d458353f57a9c0db58
11bus11 <02linfor01@gmail.com>
2025-05-12 14:59:48 +0200

    finish create copy

--- libraryfx_v1/src/main/java/library/Copy.java
+++ libraryfx_v1/src/main/java/library/Copy.java
@@ -29,7 +29,7 @@ public class Copy {
     public String getCopyStatus(){ return copyStatus; }
     public Work   getWork()      { return work; }
 
-    public static ArrayList<Copy> createCopy() {
+    public static ArrayList<Copy> createCopies() {
         DataSource dataSource = DbUtil.createDataSource();
             ArrayList <Copy> arrayCopies = new ArrayList<Copy>();
             
@@ -39,10 +39,10 @@ public class Copy {
                     int copyID = resultSet.getInt("CopyID");
                     String barcode = resultSet.getString("Barcode");
                     Work work = findCopyWork(resultSet.getString("WorkID"));
-                    String email = resultSet.getString("Email");
-                    String password = resultSet.getString("Password");
-                    String userType = resultSet.getString("UserType");
-                    Copy copy = new Copy(copyID, firstName, lastName, email, password, userType);
+                    Boolean isReference = resultSet.getBoolean("IsReference");
+                    String copyStatus = resultSet.getString("CopyStatus");
+                    String copyPlacement = resultSet.getString("CopyPlacement");
+                    Copy copy = new Copy(copyID, barcode, work, isReference, copyStatus, copyPlacement);
                     arrayCopies.add(copy);
                     System.out.println(user.lastName);
                 }
@@ -62,10 +62,10 @@ public class Copy {
     }
 
     //finding the user who made the loan
-    public static User findCopyWork(int id) {
+    public static Work findCopyWork(int id) {
         Work copyWork = null;
-        for (User i : Work.arrayWorksGlobal) {
-            if (i.userID == id) {
+        for (Work i : Work.arrayWorksGlobal) {
+            if (i.workID == id) {
                 copyWork = i;
             }
         }
